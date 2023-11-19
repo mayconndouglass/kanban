8632cdb90d08b33b121a9eae5dcaab37f17fca5a
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-11-19 09:16:10 -0300

    chore: add find by id

--- back-end/src/repositories/prisma/prisma-project-repository.ts
+++ back-end/src/repositories/prisma/prisma-project-repository.ts
@@ -3,8 +3,15 @@ import { ProjectRepository } from "../interfaces/project-repository"
 import { prisma } from "@/lib/prisma"
 
 export class PrismaProjectRepository implements ProjectRepository {
+    async findById(id: string) {
+        const project = await prisma.project.findUnique({ where: { id } })
+
+        return project
+    }
+
     async create(data: Prisma.ProjectUncheckedCreateInput): Promise<Project> {
         const project = await prisma.project.create({ data })
+
         return project
     }
 
