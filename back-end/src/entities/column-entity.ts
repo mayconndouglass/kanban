3861360a9273dfe987ebd84c666709588d268761
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-11-19 09:58:08 -0300

    refactor: column entity

--- back-end/src/entities/column-entity.ts
+++ back-end/src/entities/column-entity.ts
@@ -6,4 +6,15 @@ interface ColumnEntityProps extends BaseEntityProps {
     projectId: string
 }
 
-export class ColumnEntity extends BaseEntity<ColumnEntityProps> { }
+export class ColumnEntity extends BaseEntity<ColumnEntityProps> {
+    static create(
+        props: Omit<ColumnEntityProps, "createdAt"> & { createdAt?: Date }
+    ) {
+        const column = new ColumnEntity({
+            ...props,
+            createdAt: props.createdAt ?? new Date()
+        })
+
+        return column
+    }
+}
